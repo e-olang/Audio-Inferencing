d0d8e2eb3783cd440999cbd2d7a0a7109f695a91
eolang <64926283+e-olang@users.noreply.github.com>
2024-07-28 12:57:07 +0300

    Error handling

---
+++ helpers/whisper_inference.py
@@ -0,0 +1,22 @@
+import warnings
+import whisper
+from subprocess import CalledProcessError
+
+warnings.filterwarnings('ignore')
+
+base_model = whisper.load_model('base')
+
+def transcribe_base(filepath):
+    """
+    Args: Audio file path.
+    Supports: wav, mp3, flac
+    """
+    try:
+        result = base_model.transcribe(filepath)
+        return result['text']
+    except FileNotFoundError:
+        return 'File not found error: The specified file does not exist.'
+    except CalledProcessError:
+        return 'CalledProcessError: An error occurred while processing the file with ffmpeg.'
+    except Exception as e:
+        return f'An unexpected error occurred: {str(e)}'
